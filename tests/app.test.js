a7ce010bf4803b51d4a9f1aefadd2c3dc866356f
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-23 12:39:16 -0300

    Checking if extensions exists

--- tests/app.test.js
+++ tests/app.test.js
@@ -11,8 +11,10 @@ describe('Graphql Server', () => {
       .get('/graphql?query={test}')
       .expect('Content-Type', /json/)
       .expect(({ body }) => {
-        if (!('test' in body.data)) throw new Error('missing test');
-        if (body.data.test !== 'ok') throw new Error('query "test" not retrieve "ok" value');
+        const { data, extensions } = body;
+        if (!('test' in data)) throw new Error('missing test');
+        if (data.test !== 'ok') throw new Error('query "test" not retrieve "ok" value');
+        if (!extensions) throw new Error('missing extensions');
       })
       .expect(200);
   });
