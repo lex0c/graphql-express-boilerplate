b66e17034ea9eed20147853d94a168d9cd5532f7
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-25 18:00:47 -0300

    renamed

--- database/migrations/20190824160211-create-users-table.js
+++ database/migrations/20190824160211-create-users-table.js
@@ -5,8 +5,7 @@ export default {
       first_name: { allowNull: false, type: Sequelize.STRING(100) },
       last_name: { allowNull: false, type: Sequelize.STRING(100) },
       email: { allowNull: false, type: Sequelize.STRING(100), unique: true },
-      password: { allowNull: false, type: Sequelize.STRING(255) },
-      salt_rounds: { type: Sequelize.INTEGER, defaultValue: 10 },
+      password_hash: { allowNull: false, type: Sequelize.STRING(255) },
       created_at: { allowNull: false, type: Sequelize.DATE },
       updated_at: { allowNull: false, type: Sequelize.DATE },
     });
