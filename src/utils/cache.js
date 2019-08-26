aaa6d56503d757835cf23491b1fbd4efebd0a9b4
Léo Castro <leonardo_carvalho@outlook.com>
2019-08-26 14:33:58 -0300

    Configuring cache with Redis

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,1 +1,42 @@
-export default async (key, data) => ({ key, ...(await data) });
+import Redis from 'redis';
+import RedisMock from 'redis-mock';
+import { promisify } from 'util';
+
+import redisConfig from '../../config/redis';
+
+import { ENV_TEST } from '../constants';
+
+const redis = (process.env.NODE_ENV !== ENV_TEST) ? Redis : RedisMock;
+
+const client = redis.createClient({
+  ...redisConfig,
+  detect_buffers: true,
+  retry_strategy: (options) => {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      return new Error('The server refused the connection');
+    } else if (options.total_retry_time > (1000 * 60 * 60)) {
+      return new Error('Retry time exhausted');
+    } else if (options.attempt > 10) {
+      return undefined;
+    }
+    return Math.min((options.attempt * 100), 3000);
+  },
+});
+
+client.auth(process.env.REDIS_CLI_AUTH);
+
+client.getAsync = promisify(client.get).bind(client);
+
+client.flushAndQuit = () => {
+  client.flushall(() => {});
+  client.quit();
+};
+
+client.on('connect', () => {});
+client.on('reconnecting', () => {});
+client.on('error', (err) => {
+  global.console.error(err);
+});
+client.on('end', () => {});
+
+export default client;
